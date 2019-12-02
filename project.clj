a13ef69e7afc86b1df24c3af550baeec91855fc1
Taylor Wood <taylorwood@me.com>
2019-12-02 14:53:04 -0600

    Number goes up

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject advent-of-code "0.0.0-SNAPSHOT"
   :description "Advent of Code solutions"
   :url "https://blog.taylorwood.io"
-  :dependencies [[org.clojure/clojure "1.9.0"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/math.combinatorics "0.1.4"]
                  [quil "2.8.0"]
                  [instaparse "1.4.9"]
