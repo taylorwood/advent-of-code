9a4ef62649e824a355574a27a87a498b6262d66e
Taylor Wood <taylorwood@me.com>
2023-12-02 11:32:30 -0600

    Add 2023 day 1 solution

---
+++ src/advent_of_code/2023/1.clj
@@ -0,0 +1,53 @@
+(ns advent-of-code.2023.1
+  (:require [advent-of-code.elves :refer [day->input-lines]])
+  (:import [java.lang Character]))
+
+(defn first-digit [s]
+  (->> s
+       (re-matches #"^.*?(\d).*")
+       (second)))
+
+(defn last-digit [s]
+  (first-digit (clojure.string/reverse s)))
+
+(defn digit-combo [s]
+  (read-string (str (first-digit s) (last-digit s))))
+
+;; part 1 solution
+(apply + (map digit-combo (day->input-lines 2023 1)))
+
+(def word->digit
+  {"one" 1
+   "two" 2
+   "three" 3
+   "four" 4
+   "five" 5
+   "six" 6
+   "seven" 7
+   "eight" 8
+   "nine" 9})
+
+(defn string->digit [s]
+  (first
+   (for [[k v] word->digit
+         :when (clojure.string/starts-with? s k)]
+     v)))
+
+(defn parse-digits [s]
+  (loop [s s
+         acc []]
+    (cond
+      (empty? s) acc
+      
+      (Character/isDigit (first s))
+      (recur (subs s 1) (conj acc (read-string (str (first s)))))
+
+      :else (if-let [n (string->digit s)]
+              (recur (subs s 1) (conj acc n))
+              (recur (subs s 1) acc)))))
+
+;; part 2 solution
+(apply +
+       (for [l (day->input-lines 2023 1)]
+         (let [digits (parse-digits l)]
+           (read-string (str (first digits) (last digits))))))
